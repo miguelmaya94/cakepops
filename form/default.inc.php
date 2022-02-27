2a3b4d098a64a8651932c76633b8e907c5e6f08c
miguelmaya94 <84256870+miguelmaya94@users.noreply.github.com>
2022-02-27 01:41:29 -0800

    edits 6

--- form/default.inc.php
+++ form/default.inc.php
@@ -3,13 +3,30 @@
 
 
 
-  <div class="topnav" id="myTopnav">
-    <a href="index.html" class="active">Riverbelle Terrace</a>
-    <a href="order.html">Order Now</a>
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-    </a>
-  </div>
+
+    <nav class="navbar navbar-fixed-top navbar-expand-lg navbar-dark bg-dark ">
+        <a class="navbar-brand" href="#">Kathy's Cakes</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="order.html">Order Now</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+              
+            </ul>
+        </div>
+    </nav>
 
 
 <h1>Submission Failed missing Data</h1>
